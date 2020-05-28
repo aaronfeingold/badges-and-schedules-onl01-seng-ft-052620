fbff2c38c71b308aa79a598c0abe8e89eecb6f10
Aaron Feingold <ajfeingold88@gmail.com>
2020-05-28 23:39:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 index = 0
 def assign_rooms(attendees)
   room_assignments = []
-attendees.each_with_index { |name, index| room_assignments << "Hello, #{name}! You'll be assigned to room #{index}"}
+attendees.each_with_index { |name, i| room_assignments << "Hello, #{name}! You'll be assigned to room #{index}"}
   index += 1
   room_assignments
 end
